3ae91ce5ce7e52c34b34d7eadc4e33998d830fc1
Flatiron School <flatironschool@Flatirons-MacBook-Air-3.local>
2018-07-22 15:52:29 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Mr. Chocolate", 34, 'M', "dark brown", "Angryboi", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Rowdy", 26, 'M', "Black", "Wild", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Tabitha",24, 'F', "dark brown", "Sleek", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Sergeant Brown", 35, 'M', "Red", "MurderHobo", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Melissa", 69, 'F', "Brown", "shy", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Grinch", 13, 'M', "Blue", "Angryboi", 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Wendy", 23, 'F', "Green", "Decomposing", 0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (null, 1000, 'M', "Brown", "Angryboi", 1);
