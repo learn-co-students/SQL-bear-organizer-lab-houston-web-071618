ab5184e4aea654ce14d0e91bd1ce0d7d0b8e27c5
AngelaDiSalvo <angela.disalvo@gmail.com>
2018-07-23 21:49:03 -0500

    SQL bear organizer mod1

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Maru', 3, 'F', 'blue', 'happy', 'true');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Dog', 4, 'M', 'blue', 'sad', 'true');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Bird', 7, 'F', 'purple', 'angry', 'true');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Snake', 10, 'F', 'indigo', 'confused', 'false');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Ford', 2, 'F', 'orange', 'confused', 'true');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Annie', 7, 'F', 'purple', 'angry', 'true');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Greg', 10, 'F', 'blue', 'good', 'false');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 2, 'F', 'orange', 'fast', 'true');
