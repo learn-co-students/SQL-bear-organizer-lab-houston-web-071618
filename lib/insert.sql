3ee090e4bdb739c39fc8de22b1c86d5dd67c8e0a
Marylene Chan <marylenechan@Marylenes-MacBook-Pro-2.local>
2018-07-28 07:49:20 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+-- insert data into the db row here
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "brown", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 8, "M", "brown", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "M", "white", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 7, "M", "brown", "sad", 2);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 1, "M", "brown", "neutral", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 3, "M", "black", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 5, "F", "brown", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 5, "F", "brown", "happy", 1)
