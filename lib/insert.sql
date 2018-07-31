0cd2a0838a4045f226314bb172062033bccd5185
github username <github email address>
2018-07-30 20:22:43 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,119 @@
+INSERT INTO bears (name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES ("Mr. Chocolate",
+  2,
+  "M",
+  "brown",
+  "sweet",
+  "true"
+);
+
+INSERT INTO bears (name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES ("Rowdy",
+  1,
+  "M",
+  "black",
+  "playful",
+  "true"
+);
+
+INSERT INTO bears (name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES ("Tabitha",
+  5,
+  "F",
+  "black",
+  "sweet",
+  "true"
+);
+
+INSERT INTO bears (name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES ("Sergeant Brown",
+  8,
+  "M",
+  "brown",
+  "calm",
+  "true"
+);
+
+INSERT INTO bears (name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES ("Melissa",
+  3,
+  "F",
+  "black",
+  "sweet",
+  "true"
+);
+
+INSERT INTO bears (name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES ("Grinch",
+  2,
+  "M",
+  "red",
+  "aggressive",
+  "true"
+);
+
+INSERT INTO bears (name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES ("Wendy",
+  5,
+  "F",
+  "black",
+  "sweet",
+  "true"
+);
+
+INSERT INTO bears (name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (NULL,
+  9,
+  "M",
+  "red",
+  "unknown",
+  "false"
+);
